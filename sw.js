9ffd2d853dbf2fdc9a846594527afc22799116dd
samuraiscene-alt <samuraiscene@gmail.com>
2026-09-20 14:22:37 +0900

    Bump PWA cache for dice click hotfix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'klb-v42-playloop1';
+const VERSION = 'klb-v42-playloop1-hotfix1';
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE_ASSETS = [
