4bf8041bedc4ca82e9f23ceecdb2f0282c343a12
samuraiscene-alt <samuraiscene@gmail.com>
2026-09-20 12:34:00 +0900

    Bump PWA cache for V42 play loop stability

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'klb-v41-playtest-ui2';
+const VERSION = 'klb-v42-playloop1';
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE_ASSETS = [
