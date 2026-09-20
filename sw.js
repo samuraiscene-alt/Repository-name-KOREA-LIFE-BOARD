9e5a0e3d5b896977ddf408ad0c4a06caa7823f5a
samuraiscene-alt <samuraiscene@gmail.com>
2026-09-20 18:26:48 +0900

    Bump PWA cache for V49 direct roll tap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'klb-v48-roll-unlock';
+const VERSION = 'klb-v49-direct-roll-tap';
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE_ASSETS = [
